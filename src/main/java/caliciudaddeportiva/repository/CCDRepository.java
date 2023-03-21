3ce9ce7a0c1d77fb1976772f2fc8be69bf75b8c9
edward stewen <stewengilemp@gmail.com>
2023-03-21 15:49:08 -0500

    cambios

--- src/main/java/caliciudaddeportiva/repository/CCDRepository.java
+++ src/main/java/caliciudaddeportiva/repository/CCDRepository.java
@@ -296,7 +296,7 @@ public class CCDRepository {
     public int  buscarclustervalidados2 (UserDto userDto) {
         String sql = "SELECT COUNT(variable1) " +
                 "FROM usuarios " +
-                "WHERE UPPER(variable1) = UPPER(?) and evento='carreramujerbene' "
+                "WHERE UPPER(variable1) = UPPER(?) and evento='carreramujerbene' and variable9!='preregistro' "
                 ;
 
         return template.queryForObject(sql, new Object[]{userDto.getVariable1()}, Integer.class);
