1a00f53c949b1cbe6d252cc5cd2de5ddd95fe8cb
Edward <stewengil@gmail.com>
2025-03-15 07:20:23 -0500

    canmdasd

--- src/main/java/caliciudaddeportiva/service/impl/EmailService.java
+++ src/main/java/caliciudaddeportiva/service/impl/EmailService.java
@@ -1,21 +1,11 @@
 package caliciudaddeportiva.service.impl;
 
 import caliciudaddeportiva.micellaneus.dto.EmailRequestDto;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
+
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import com.itextpdf.io.image.ImageData;
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.geom.PageSize;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Image;
-import com.itextpdf.layout.element.Paragraph;
+
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 import caliciudaddeportiva.micellaneus.dto.EmailRequestDto;
